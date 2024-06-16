e6acaee149062b5c829982d4b6fc186d0ae6ad5c
nartsisss <nartsiss@proton.me>
2024-06-17 01:07:27 +0300

    feat(home): configure yazi

--- home/modules/yazi.nix
+++ home/modules/yazi.nix
@@ -2,5 +2,13 @@
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
+
+    settings = {
+      manager = {
+        sort_by = "natural";
+        show_hidden = false;
+        sort_dir_first = true;
+      };
+    };
   };
 }
