831fa9430e987c519fe2f0268ef97ceabbf450ef
nartsisss <nartsiss@proton.me>
2024-06-20 00:36:36 +0300

    feat(home): configure ssh

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -1,6 +1,14 @@
 {
   programs.ssh = {
     enable = true;
+    
     addKeysToAgent = "confirm";
+
+    extraConfig = ''
+      Host github.com
+        AddKeysToAgent yes
+        UseKeychain yes
+        IdentityFile ~/.ssh/id_ed25519
+    '';
   };
 }
