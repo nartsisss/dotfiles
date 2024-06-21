3b36db10f04b3af03ad2417ee98aa93de260ff56
nartsisss <nartsiss@proton.me>
2024-06-22 02:59:59 +0300

    chore(home): add flake update alias, remove sudo

--- home/darwin/home.nix
+++ home/darwin/home.nix
@@ -14,10 +14,11 @@
       shellAliases = {
         #todo: use hostname as flake configuration name
         rebuild = "darwin-rebuild switch --flake ~/.nix#helios";
+        update = "nix flake update ~/.nix";
         #todo: use path as variable
-        flake = "sudo $EDITOR ~/.nix/flake.nix";
-        conf = "sudo $EDITOR ~/.nix/hosts/helios/configuration.nix";
-        hm = "sudo $EDITOR ~/.nix/home/darwin/home.nix";
+        flake = "$EDITOR ~/.nix/flake.nix";
+        conf = "$EDITOR ~/.nix/hosts/helios/configuration.nix";
+        hm = "$EDITOR ~/.nix/home/darwin/home.nix";
       };
     };
   };
