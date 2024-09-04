c69107cd05749e785879b3c7d52f66fc0a0b2177
nartsisss <nartsiss@proton.me>
2024-09-04 21:34:54 +0300

    feat(nix-darwin): configure brew opts with env vars

--- modules/darwin/brew.nix
+++ modules/darwin/brew.nix
@@ -21,6 +21,12 @@ in
 {
   environment.systemPath = [ "/opt/homebrew/bin" ];
 
+  environment.variables = {
+    HOMEBREW_NO_ENV_HINTS = "1";
+    HOMEBREW_NO_ANALYTICS = "1";
+    HOMEBREW_BAT = "1";
+  };
+
   nix-homebrew = {
     enable = true;
 
