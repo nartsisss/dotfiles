d1c1e6f7010c4b58882f1898f9735a5cd8586368
nartsisss <nartsiss+git@proton.me>
2024-10-08 01:08:49 +0300

    chore(home): move ssh credentials into user config

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -3,19 +3,5 @@
     enable = true;
 
     addKeysToAgent = "confirm";
-
-    extraConfig = ''
-      Host github.com
-        AddKeysToAgent yes
-        UseKeychain yes
-        PreferredAuthentications publickey
-        IdentityFile ~/.ssh/github_ed25519
-
-      Host gitlab.com
-        AddKeysToAgent yes
-        UseKeychain yes
-        PreferredAuthentications publickey
-        IdentityFile ~/.ssh/gitlab_ed25519
-    '';
   };
 }
--- modules/home/users/nartsiss.nix
+++ modules/home/users/nartsiss.nix
@@ -1,11 +1,29 @@
 { config, ... }:
 {
-  programs.git = {
-    userName = "nartsisss";
-    userEmail = "nartsiss+git@proton.me";
+  programs = {
+    git = {
+      userName = "nartsisss";
+      userEmail = "nartsiss+git@proton.me";
 
-    signing = {
-      key = "${config.home.homeDirectory}/.ssh/git_ed25519";
+      signing = {
+        key = "${config.home.homeDirectory}/.ssh/git_ed25519";
+      };
+    };
+
+    ssh = {
+      extraConfig = ''
+        Host github.com
+          AddKeysToAgent yes
+          UseKeychain yes
+          PreferredAuthentications publickey
+          IdentityFile ~/.ssh/github_ed25519
+
+        Host gitlab.com
+          AddKeysToAgent yes
+          UseKeychain yes
+          PreferredAuthentications publickey
+          IdentityFile ~/.ssh/gitlab_ed25519
+      '';
     };
   };
 }
